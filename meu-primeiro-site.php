752609a397a9553e0ad461310da4becef678f01b
marciorecife <marciosouza1986@gmail.com>
2022-05-30 12:48:38 -0300

    Commit inicial v1

---
+++ meu-primeiro-site.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Primeiro Site em PHP! Parabens!</title>
+    </head>
+
+    <body>
+        <?php
+        for ($i = 0 ; $i <10 ; $i++){
+            print ("Linha numero " . $i . "<br />");
+        }
+        ?>
+    </body>
+
+</html>
